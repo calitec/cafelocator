c93f197e332e62dcade19f3db8ca20486f124673
AP\seoyeo <syg0713@gmail.com>
2022-01-13 18:37:35 +0900

    test

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 3000;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+const express = require('express')
+const favicon = require('express-favicon')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
+app.use(favicon(__dirname + '/build/favicon.ico'))
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'build')))
+
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  res.set('Content-Type', 'text/javascript')
+  next()
+})
+
+app.get('*.css', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  res.set('Content-Type', 'text/css')
+  next()
+})
+
 app.listen(port, () => {
-  console.log('express running on port 3000');
-});
+  console.log('express running on port 3000')
+})
