8a5605dccebfe8909f03aef0921435dd23200c87
AP\seoyeo <syg0713@gmail.com>
2022-01-17 18:32:01 +0900

    test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const favicon = require('express-favicon')
+const compression = require('compression')
 const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
@@ -7,6 +8,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'))
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
+app.use(compression())
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
