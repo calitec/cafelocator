ef659fb38737f1e1cc41026535ac391a48ae8439
calininja <syg0713@gmail.com>
2022-02-01 18:02:17 +0900

    test

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
 
-
+app.get('*.js', function(req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+ });
+ 
+ app.get('*.css', function(req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/css');
+  next();
+ });
+ 
 app.use(favicon(__dirname + '/build/favicon.ico'))
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname))
