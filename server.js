63c853402f8c60585ebcae8761dcf3a3cfd5b3bb
calininja <syg0713@gmail.com>
2022-02-01 18:20:10 +0900

    test

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 const express = require('express')
-const favicon = require('express-favicon')
+// const favicon = require('express-favicon')
 const compression = require('compression')
 const path = require('path')
 const port = process.env.PORT || 3000
+const morgan = require('morgan')
 const app = express()
-
-app.get('*.js', function(req, res, next) {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  res.set('Content-Type', 'text/javascript');
-  next();
- });
+const dev = app.get('env') !== 'production'
+// app.get('*.js', function(req, res, next) {
+//   req.url = req.url + '.gz';
+//   res.set('Content-Encoding', 'gzip');
+//   res.set('Content-Type', 'text/javascript');
+//   next();
+//  });
  
- app.get('*.css', function(req, res, next) {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  res.set('Content-Type', 'text/css');
-  next();
- });
- 
-app.use(favicon(__dirname + '/build/favicon.ico'))
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'build')))
-app.use(compression())
+//  app.get('*.css', function(req, res, next) {
+//   req.url = req.url + '.gz';
+//   res.set('Content-Encoding', 'gzip');
+//   res.set('Content-Type', 'text/css');
+//   next();
+//  });
+ if(!dev) {
+  app.disable('x-powered-by')
+  app.use(compression())
+  app.use(morgan('common'))
+  // app.use(favicon(__dirname + '/build/favicon.ico'))
+  // the __dirname is the current directory from where the script is running
+  // app.use(express.static(__dirname))
+  app.use(express.static(path.join(__dirname, 'build')))
+  
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
+ }
+ if(dev) {
+  app.use(morgan('dev'))
+} 
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
 app.listen(port, () => {
   console.log('express running on port 3000')
 })
