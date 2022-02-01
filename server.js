9b291c6ddb56dfdaa4c86e79ae7cf28126e29e7b
calininja <syg0713@gmail.com>
2022-02-01 18:26:55 +0900

    test

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 const express = require('express')
-// const favicon = require('express-favicon')
 const compression = require('compression')
 const path = require('path')
 const port = process.env.PORT || 3000
 const morgan = require('morgan')
 const app = express()
 const dev = app.get('env') !== 'production'
-// app.get('*.js', function(req, res, next) {
-//   req.url = req.url + '.gz';
-//   res.set('Content-Encoding', 'gzip');
-//   res.set('Content-Type', 'text/javascript');
-//   next();
-//  });
- 
-//  app.get('*.css', function(req, res, next) {
-//   req.url = req.url + '.gz';
-//   res.set('Content-Encoding', 'gzip');
-//   res.set('Content-Type', 'text/css');
-//   next();
-//  });
+
  if(!dev) {
   app.disable('x-powered-by')
   app.use(compression())
   app.use(morgan('common'))
-  // app.use(favicon(__dirname + '/build/favicon.ico'))
   // the __dirname is the current directory from where the script is running
-  // app.use(express.static(__dirname))
   app.use(express.static(path.join(__dirname, 'build')))
   
   app.get('/*', function (req, res) {
